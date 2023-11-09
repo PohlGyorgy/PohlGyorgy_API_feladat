5ae419a60733594d6b403b45602c55d83770de38
Pohl György <76537494129@szily.lan>
2023-11-09 10:33:46 +0100

    javascript külön fájlba tétel

---
+++ request.js
@@ -0,0 +1,20 @@
+function loadDoc() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.onreadystatechange = function () {
+        if (this.readyState == 4 && this.status == 200) {
+            const myObj = JSON.parse(this.responseText);
+            document.getElementById("nev").value = myObj.results[0].name.title + " " + myObj.results[0].name.first + " " + myObj.results[0].name.last;
+            document.getElementById("nem").value = myObj.results[0].gender;
+            document.getElementById("szuletes").value = myObj.results[0].dob.date;
+            document.getElementById("orszag").value = myObj.results[0].location.country;
+            document.getElementById("varos").value = myObj.results[0].location.city;
+            document.getElementById("email").value = myObj.results[0].email;
+            document.getElementById("telefon").value = myObj.results[0].phone;
+            document.getElementById("username").value = myObj.results[0].login.username;
+            document.getElementById("pw").value = myObj.results[0].login.password;
+            document.getElementById("profilkep").src = myObj.results[0].picture.thumbnail;
+        }
+    };
+    xhttp.open("GET", "https://randomuser.me/api/", true);
+    xhttp.send();
+}
